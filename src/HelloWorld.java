c2a93de91ce629e96d49715c4abc06b6b2a51c70
SemenovAS <jazziq@mail.ru>
2021-02-03 17:11:07 +0200

    HelloWorld lesson 1

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        args = new String[2];
+        args[0] = "Hello";
+        args[1] = "World";
+        HelloWorldLib.printArray(args);
+    }
+}
---
+++ src/HelloWorldLib.java
@@ -0,0 +1,9 @@
+public class HelloWorldLib {
+
+    public static void printArray(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+
+}
